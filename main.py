5bbd592ab60ccaa1b5b9c9620328b36136c13d51
sebas <sebasucedo@gmail.com>
2024-03-29 13:52:52 -0300

    to aws lambda function

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 import os
 import requests
 from openai import OpenAI
+import json
+
+def lambda_handler(event, context):
+
+  client = OpenAI()
+
+  completion = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+      {"role": "system", "content": "You are a frustrated developer who became a scrum master, you think you are the ultimate ontological coach but you really have no experience in anything."},
+      {"role": "user", "content": "Write an inspirational phrase for a development team based on the current day of the week."}
+    ]
+  )
+
+  mensaje = completion.choices[0].message.content
+
+
+  ids_chats = obtener_ids_chats()
+
+  ids_directos = ids_chats['directos']
+  for chat_id in ids_directos:
+      chat_id_str = str(chat_id)
+      respuesta = enviar_mensaje_telegram(chat_id_str, mensaje)
+
+  # ids_grupos = ids_chats['grupos']
+  # for chat_id in ids_grupos:
+  #     chat_id_str = str(chat_id)
+  #     respuesta = enviar_mensaje_telegram(chat_id_str, mensaje)
+
+
+
+  return {
+      'statusCode': 200,
+      'body': json.dumps('Hello from Lambda!')
+  }
+
 
 TOKEN_TELEGRAM = os.getenv('TELEGRAM_TOKEN')
 URL_TELEGRAM_UPDATES= f"https://api.telegram.org/bot{TOKEN_TELEGRAM}/getUpdates"
@@ -39,40 +75,10 @@ def obtener_ids_chats():
 
 def enviar_mensaje_telegram(chat_id, mensaje):
     url = f"https://api.telegram.org/bot{TOKEN_TELEGRAM}/sendMessage"
-    datos = {"chat_id": chat_id, "text": mensaje}
+    payload = {"chat_id": chat_id, "text": mensaje}
 
-    response = requests.post(url, data=datos)
+    response = requests.post(url, data=payload)
     return response.json()
 
 
-
-
-
-client = OpenAI()
-
-completion = client.chat.completions.create(
-  model="gpt-4",
-  messages=[
-    {"role": "system", "content": "You are a frustrated developer who became a scrum master, you think you are the ultimate ontological coach but you really have no experience in anything."},
-    {"role": "user", "content": "Write an inspirational phrase for a development team based on the current the day of the week."}
-  ]
-)
-
-mensaje = completion.choices[0].message.content
-# print(mensaje)
-
-
-
-ids_chats = obtener_ids_chats()
-
-ids_directos = ids_chats['directos']
-for chat_id in ids_directos:
-    chat_id_str = str(chat_id)
-    respuesta = enviar_mensaje_telegram(chat_id_str, mensaje)
-    # print(f"Respuesta para el chat {chat_id_str}: {respuesta}")
-
-ids_grupos = ids_chats['grupos']
-for chat_id in ids_grupos:
-    chat_id_str = str(chat_id)
-    respuesta = enviar_mensaje_telegram(chat_id_str, mensaje)
-    # print(f"Respuesta para el chat {chat_id_str}: {respuesta}")
+# lambda_handler(None, None)
