3709319e4c11a27714bb985ad5050e7184c342c7
sebas <sebasucedo@gmail.com>
2024-04-14 15:34:52 -0300

    Message handler

--- webhook/lambda_function.py
+++ webhook/lambda_function.py
@@ -9,14 +9,24 @@ def lambda_handler(event, context):
     chat_id = message["message"]["chat"]["id"]
     incoming_text = message["message"].get("text", "")
     
-    response_text = f"Recibí tu mensaje: {incoming_text}"
-
-    send_response_to_telegram(chat_id, response_text)
-
+    handle_command(chat_id, incoming_text)
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Mensaje procesado correctamente')
     }
+
+def handle_command(chat_id, message_text):
+    if message_text.startswith('/'):
+        if message_text == '/start':
+            response_text = "Hello! I am a bot that will respond to any message you send me."
+        else:
+            response_text = f"Unrecognized command: {message_text}"
+        send_response_to_telegram(chat_id, response_text)
+    else:
+        response_text = f"Recibí tu mensaje: {message_text}"
+        send_response_to_telegram(chat_id, response_text)
+
 
 def send_response_to_telegram(chat_id, text):
     url = f"https://api.telegram.org/bot{TOKEN_TELEGRAM}/sendMessage"
