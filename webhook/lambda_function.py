9e3099715c13a9aa8a1bd7817615d563ff677d51
sebas <sebasucedo@gmail.com>
2024-04-14 18:44:59 -0300

    copy all .py files

--- webhook/lambda_function.py
+++ webhook/lambda_function.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import json
+from datetime import datetime
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from utils.ai import get_response
@@ -24,6 +25,10 @@ def handle_command(chat_id, message_text):
     if message_text.startswith('/'):
         if message_text == '/start':
             response_text = "Hello! I am a bot that will respond to any message you send me."
+        if message_text.startswith('/settime '):
+            time_string = message_text.split('/settime ')[1].strip()
+            time_obj = datetime.strptime(time_string, '%H:%M').time()
+            schedule_event(time_obj)
         else:
             response_text = f"Unrecognized command: {message_text}"
         send_message(chat_id, response_text)
@@ -32,3 +37,9 @@ def handle_command(chat_id, message_text):
         send_message(chat_id, response_text)
 
 
+def schedule_event(time_obj):
+
+    #TODO: Implement event scheduling logic here
+
+    time_str = time_obj.strftime('%H:%M')
+    print("Scheduling event for: {}".format(time_str))
