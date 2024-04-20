2bc76027bdaa02632a964f3dcd16c4e8791bbc0d
sebas <sebasucedo@gmail.com>
2024-04-20 18:30:34 -0300

    fix test_event and cron

--- tests/test_webhook.py
+++ tests/test_webhook.py
@@ -9,20 +9,28 @@ from webhook.lambda_function import lambda_handler
 class TestWebhook:
   def test_handler_correct_body(self):
     test_event = {
-      "body": json.dumps({
-          "update_id": 10000,
-          "message": {
-              "chat": {
-                  "id": 1111,
-                  "first_name": "John",
-                  "last_name": "Doe",
-                  "type": "private"
-              },
-              "date": 1441645532,
-              "text": "/settime 10:00"
-          }
+        "body": json.dumps({
+            "update_id": 10000,
+            "message": {
+                "from": {
+                    "id": 1111,
+                    "is_bot": False,
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "username": "johndoe",
+                    "language_code": "en"
+                },
+                "chat": {
+                    "id": 1111,
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "type": "private"
+                },
+                "date": 1441645532,
+                "text": "/settime 10:00"
+            }
         })
-      }
+    }
 
     response = lambda_handler(test_event, None)
 
